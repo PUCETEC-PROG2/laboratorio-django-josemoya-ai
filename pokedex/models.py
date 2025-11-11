87d7b79f53358414193d9a2cd57d97b018bc6dd4
josemoya-ai <jose.moya@uisek.edu.ec>
2025-11-11 13:18:35 -0500

    Finalizacion de laboratorio 5.2

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
+### Entrenador
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    level = models.IntegerField(default=1)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 # Create your models here.
 class Pokemon(models.Model):
     name = models.CharField(max_length=30, null=False)
     type = models.CharField(max_length=30, null=False)
     weight = models.DecimalField(decimal_places=4, max_digits=6)
     height = models.DecimalField(decimal_places=4, max_digits=6)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
     picture = models.ImageField(upload_to='pokemons/', null=False, blank=True)
 
     def __str__(self):
         return self.name
-
-### Entrenador
-#class Trainer(models.Model):
-    # Nombre
-    # Apellido
-    # Nivel
-    # Fecha de nacimiento DateField()
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
+from django.shortcuts import redirect, render
+from pokedex.forms import PokemonForm
 
 def index(request):
     pokemons = Pokemon.objects.all()
+    trainers = Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+            'pokemons': pokemons,
+            'trainers' : trainers
+        },
+        request))
 
 def pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id = pokemon_id)
@@ -14,3 +21,37 @@ def pokemon(request, pokemon_id):
         'pokemon': pokemon
     }
     return HttpResponse(template.render(context, request))
+
+def trainer_details(request, trainer_id):
+    trainer = Trainer.objects.get(id = trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form' : form})
+
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form' : form})
+
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    pokemon.delete()
+    return redirect('pokedex:index')
