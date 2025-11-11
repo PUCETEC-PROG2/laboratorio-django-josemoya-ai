8f4aacb94787c8d4a5651e31e3196db35ad69a63
josemoya-ai <jose.moya@uisek.edu.ec>
2025-11-10 23:18:36 -0500

    Finalizacion de lavoratorio 5.1

--- pokedex/models.py
+++ pokedex/models.py
@@ -6,12 +6,14 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=30, null=False)
     weight = models.DecimalField(decimal_places=4, max_digits=6)
     height = models.DecimalField(decimal_places=4, max_digits=6)
+    picture = models.ImageField(upload_to='pokemons/', null=False, blank=True)
 
     def __str__(self):
         return self.name
 
 ### Entrenador
-# Nombre
-# Apellido
-# Nivel
-# Fecha de nacimiento DateField()
+#class Trainer(models.Model):
+    # Nombre
+    # Apellido
+    # Nivel
+    # Fecha de nacimiento DateField()
