75d15425b85903598b393f560529f93ed1c95a77
josemoya-ai <jose.moya@uisek.edu.ec>
2025-11-10 20:49:58 -0500

    Finlalizar laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+
+    def __str__(self):
+        return self.name
+
+### Entrenador
+# Nombre
+# Apellido
+# Nivel
+# Fecha de nacimiento DateField()
